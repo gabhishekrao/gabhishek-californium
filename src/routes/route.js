d3711b566d90d27082afded6e92ffcfe28c32c30
gabhishekrao <g.abhishek.raoo@gmail.com>
2022-12-14 18:28:27 +0530

    added file

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
+/* 
 router.get('/students/:name', function(req, res) {
     let studentName = req.params.name
     console.log(studentName)
@@ -158,6 +158,49 @@ router.post( "/post-query-2", function (req, res){
     }
     res.send( {data: finalArr , status: true})
 })
+ */
 
+
+ let persons = [
+    {
+        name: "PK",
+        age: 10,
+        votingStatus: false
+    },
+    {
+        name: "SK",
+        age: 20,
+        votingStatus: false
+    },
+    {
+        name: "AA",
+        age: 70,
+        votingStatus: false
+    },
+    {
+        name: "SC",
+        age: 5,
+        votingStatus: false
+    },
+    {
+        name: "HO",
+        age: 40,
+        votingStatus: false
+    }
+]
+
+
+router.post("/age", function (req, res) {
+    let votingAge = req.query.votingAge
+    let arr = persons.filter(ele => ele.age >= votingAge)
+    let finalArr = arr.filter(personStatus => personStatus.votingStatus = true)
+
+
+    res.send({ data: finalArr, status: true })
+})
 
 module.exports = router;
+
+
+
+
