a358c84ae81e6c157be1651470b2849e224a0e7d
gabhishekrao <g.abhishek.raoo@gmail.com>
2022-12-23 22:57:11 +0530

    Added

--- src/index.js
+++ src/index.js
@@ -1,27 +1,34 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
-const { default: mongoose } = require('mongoose');
+
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
-    useNewUrlParser: true
-})
-.then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
-
 app.use('/', route);
-// unreachable becuase the cycle has terminated
-app.use(
-    function (req, res, next) {
-        console.log ("inside GLOBAL MW");
-        next();
-  }
-);
+ 
+app.use(function (req, res, next) {
+    
+    var currentdate = new Date(); 
+    var datetime =  currentdate.getDate() + " "
+                    + (currentdate.getMonth()+1)  + " " 
+                    + currentdate.getFullYear() + "  "  
+                    + currentdate.getHours() + ":"  
+                    + currentdate.getMinutes() + ":" 
+                    + currentdate.getSeconds();
+
+    let ip= req.ip
+    let url= req.originalUrl
+
+    console.log(`${datetime}  ${ip}  ${url}`)
+
+    res.send({msg: "Get back to console"})
+    next()    
+    
+});
 
 
 app.listen(process.env.PORT || 3000, function () {
